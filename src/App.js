58a972f6590296caab4d2318c385d1701f172f25
Gortenzik <gortenzik@yandex.ru>
2022-11-09 11:13:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React from "react";
+import { useEffect, useState} from 'react';
 import './App.css';
+import { gsap } from 'gsap';
+import imageOne from './minitotoro.png'
+
+const { useRef } = React;
 
 function App() {
+
+  const [myTip, setMyTip] = useState('');
+  const [nextTip, setNextTip] = useState('');
+  
+  const boxRef = useRef();
+
+
+  useEffect (() => {
+    async function Tip() {
+      const response = await fetch (`http://www.boredapi.com/api/activity/`);
+      const data = await response.json();
+      setMyTip(data.activity);
+    }
+    Tip();
+  }, [nextTip])
+
+
+  useEffect(() => {
+    gsap.to(boxRef.current, {
+        x: -1000,
+        delay: 1,
+        opacity: 0,
+        duration: 17, 
+    })
+})
+
+
+  const reboot = (e) => {
+    e.preventDefault();
+    setNextTip(myTip);
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+       <div className="animImg">
+          <img ref={boxRef} src={imageOne} alt='' width='350px'/>
+        </div>
+
+        <div className="header">
+          <h1>Tip of the day</h1>
+        <div className='inpBtn' onSubmit={reboot}>
+          <p className="text-typing" >{myTip}</p>
+          <button className="btnSearch" onClick={() => {setNextTip(myTip)} }>Next tip</button>
+        </div>
+        </div>
+      </div>
   );
 }
 
